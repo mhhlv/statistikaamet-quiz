95cd61b8d826a3dd247d5e6bb71d3792d090873c
Mihhail Mihhailov <92859606+mhhlv@users.noreply.github.com>
2026-03-25 16:03:07 +0200

    write custom state aachine for easier handling

--- services/quiz/manager.ts
+++ services/quiz/manager.ts
@@ -1,14 +1,44 @@
 import type * as Quiz from "./types";
-import { stateMachine } from "./state-machine";
-import { type Actor, createActor } from "xstate";
 
 export class QuizManager {
   quiz: Quiz.Quiz;
-  state: Actor<typeof stateMachine>;
+  state: (typeof QuizManager.states)[keyof typeof QuizManager.states];
+  currentQuestion: number;
+
+  private static states = {
+    INTRODUCTION: "INTRODUCTION",
+    QUESTION: "QUESTION",
+    QUESTION_RESULT: "QUESTION_RESULT",
+    FINAL_RESULT: "FINAL_RESULT",
+  } as const;
 
   constructor(quiz: Quiz.Quiz) {
     this.quiz = quiz;
-    this.state = createActor(stateMachine, {input: {questionCount: quiz.questions.length}});
+    this.state = QuizManager.states.INTRODUCTION;
+    this.currentQuestion = 0;
+  };
+
+  private transition(): void {
+    switch (this.state) {
+      case QuizManager.states.INTRODUCTION:
+        this.state = QuizManager.states.QUESTION;
+        break;
+      case QuizManager.states.QUESTION:
+        this.state = QuizManager.states.QUESTION_RESULT;
+        break;
+      case QuizManager.states.QUESTION_RESULT:
+        if (this.currentQuestion < this.quiz.questions.length - 1) {
+          this.state = QuizManager.states.QUESTION;
+          this.currentQuestion++;
+        } else {
+          this.state = QuizManager.states.FINAL_RESULT;
+        };
+        break;
+      case QuizManager.states.FINAL_RESULT:
+        this.state = QuizManager.states.INTRODUCTION;
+        this.currentQuestion = 0;
+        break;
+    };
   };
 
 };
