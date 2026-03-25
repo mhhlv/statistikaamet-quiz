7f46a7b30d62480851344b230352f5209c8c3b24
Mihhail Mihhailov <92859606+mhhlv@users.noreply.github.com>
2026-03-25 19:02:59 +0200

    refactor logic out of the switch statement

--- services/quiz/manager.ts
+++ services/quiz/manager.ts
@@ -14,25 +14,20 @@ export class QuizManager {
   advance(event: unknown): void {
     switch (this.state) {
       case QuizManager.states.INTRODUCTION:
-        this.state = QuizManager.states.QUESTION;
+        this.goToNextQuestion();
         break;
       case QuizManager.states.QUESTION:
-        const [isValid, value] = this.isAnswerIDValid(event);
-        if (isValid) {
-          this.givenAnswers.push(value!);
-          this.state = QuizManager.states.QUESTION_RESULT;
-        };
+        this.submitAnswer(event);
         break;
       case QuizManager.states.QUESTION_RESULT:
-        if (this.givenAnswers.length < this.quiz.questions.length) {
-          this.state = QuizManager.states.QUESTION;
+        if (this.hasNextQuestion()) {
+          this.goToNextQuestion();
         } else {
-          this.state = QuizManager.states.FINAL_RESULT;
+          this.goToFinalResult();
         };
         break;
       case QuizManager.states.FINAL_RESULT:
-        this.givenAnswers.length = 0;
-        this.state = QuizManager.states.INTRODUCTION;
+        this.resetQuiz();
         break;
     };
   };
@@ -45,16 +40,42 @@ export class QuizManager {
   } as const;
 
   private isAnswerIDValid(event: unknown): [boolean, string?] {
+    const answer = (event as Record<string, string>)["answer"];
     const validAnswerIDs = this.quiz.questions[this.givenAnswers.length]!.answers.flatMap(
       (answer) => { return answer.id; }
     );
-
-    const answer = (event as Record<string, string>)["answer"];
     if (answer && answer in validAnswerIDs) {
       return [true, answer];
     }
-
     return [false];
+  };
+
+  private hasNextQuestion(): boolean {
+    if (this.givenAnswers.length < this.quiz.questions.length) {
+      return true;
+    };
+    return false;
+  };
+
+  private resetQuiz(): void {
+    this.givenAnswers.length = 0;
+    this.state = QuizManager.states.INTRODUCTION;
+  };
+
+  private goToNextQuestion(): void {
+    this.state = QuizManager.states.QUESTION;
+  };
+
+  private submitAnswer(event: unknown): void {
+    const [isValid, value] = this.isAnswerIDValid(event);
+    if (isValid) {
+      this.givenAnswers.push(value!);
+      this.state = QuizManager.states.QUESTION_RESULT;
+    };
+  };
+
+  private goToFinalResult(): void {
+    this.state = QuizManager.states.FINAL_RESULT;
   };
 
 };
