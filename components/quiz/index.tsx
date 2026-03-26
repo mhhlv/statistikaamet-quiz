146bb40cf5cc1d44780eb53521e33244386acd66
Mihhail Mihhailov <92859606+mhhlv@users.noreply.github.com>
2026-03-26 23:43:15 +0200

    structure of quiz react component

--- components/quiz/index.tsx
+++ components/quiz/index.tsx
@@ -9,7 +9,22 @@ export type Props = {
 export default function Quiz(props: Props) {
   return (
     <section>
-      <h1>{props.manager.title()}</h1>
+      <h1>{ props.manager.title() }</h1>
+      {
+        props.manager.currentPage().text.map(
+          (paragraph) => { return <p>{ paragraph.text }</p>; } 
+        )
+      }
+      {
+        props.manager.currentPage().answers?.map(
+          (answer) => { return <button id={ answer.id }>{ answer.text }</button> }
+        )
+      }
+      {
+        props.manager.currentPage().button
+          ? <button>{ props.manager.currentPage().button?.text }</button>
+          : null
+      }
     </section>
   );
 };
