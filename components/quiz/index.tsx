5fa87ee0e1d665e973d3dcfb47774a0b3eda6a68
Mihhail Mihhailov <92859606+mhhlv@users.noreply.github.com>
2026-03-27 17:55:38 +0200

    use more radix components

--- components/quiz/index.tsx
+++ components/quiz/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Heading, Text, Button } from "@radix-ui/themes";
+import { Card, Heading, Text, Button, Table } from "@radix-ui/themes";
 
 import { type Quiz } from "@/services/quiz/format";
 import { QuizManager } from "@/services/quiz/manager";
@@ -17,47 +17,43 @@ export default function Quiz({ data }: { data: Quiz }) {
   };
 
   return (
-    <section>
+    <Card>
       <Heading>{ page.title.text }</Heading>
       <Text>{page.paragraph.text}</Text>
-      {
-        page.answers?.map(
-          answer => (
-            <Button onClick={() => handleUserAction({ answer: answer.id }) }>
-              { answer.text }
-            </Button>
-          )
-        )
-      }
-      {
-        page.table
-          ? <table>
-              <tr>
-                <th>{ page.table.headers.questions.text }</th>
-                <th>{ page.table.headers.correctAnswers.text }</th>
-                <th>{ page.table.headers.givenAnswers.text }</th>
-              </tr>
-              {
-                page.table.rows.map(
-                  row => (
-                    <tr>
-                      <th>{row.questions.text}</th>
-                      <th>{row.correctAnswers.text}</th>
-                      <th>{row.givenAnswers.text}</th>
-                    </tr>
-                  )
-                )
-              }
-            </table>
-          : null
-      }
-      {
-        page.button
-          ? <Button onClick={ () => handleUserAction({}) }>
-              { page.button?.text }
-            </Button>
-          : null
-      }
-    </section>
+
+      {page.answers?.map((answer) => (
+        <Button key={answer.id} onClick={() => handleUserAction({ answer: answer.id })}>
+          {answer.text}
+        </Button>
+      ))}
+
+      {page.table && (
+        <Table.Root>
+          <Table.Header>
+            <Table.Row>
+              <Table.ColumnHeaderCell>{page.table.headers.questions.text}</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>{page.table.headers.correctAnswers.text}</Table.ColumnHeaderCell>
+              <Table.ColumnHeaderCell>{page.table.headers.givenAnswers.text}</Table.ColumnHeaderCell>
+            </Table.Row>
+          </Table.Header>
+          <Table.Body>
+            {page.table.rows.map((row, i) => (
+              <Table.Row key={i}>
+                <Table.Cell>{row.questions.text}</Table.Cell>
+                <Table.Cell>{row.correctAnswers.text}</Table.Cell>
+                <Table.Cell>{row.givenAnswers.text}</Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+      )}
+
+
+      {page.button && (
+        <Button onClick={() => handleUserAction({})}>
+          {page.button.text}
+        </Button>
+      )}
+    </Card>
   );
 };
