dfa424375ac2193a42d55ba23a8852c9ddd3e3cd
Mihhail Mihhailov <92859606+mhhlv@users.noreply.github.com>
2026-03-27 20:27:26 +0200

    styling done

--- components/quiz/index.tsx
+++ components/quiz/index.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { Card, Heading, Text, Button, Table } from "@radix-ui/themes";
+import { Heading, Text, Button, Table, Section, Grid, Container, Flex, Card } from "@radix-ui/themes";
 
 import { type Quiz } from "@/services/quiz/format";
 import { QuizManager } from "@/services/quiz/manager";
@@ -16,43 +16,70 @@ export default function Quiz({ data }: { data: Quiz }) {
   };
 
   return (
-    <Card>
-      <Heading>{ page.title.text }</Heading>
-      <Text>{page.paragraph.text}</Text>
+    <Container size="3">
+      <Card>
+        <Flex height="90vh" direction="column">
+          <Section py="3">
+            <Heading>{page.title.text}</Heading>
+          </Section>
 
-      {page.answers?.map((answer) => (
-        <Button key={answer.id} onClick={() => handleUserAction({ answer: answer.id })}>
-          {answer.text}
-        </Button>
-      ))}
+          <Section py="3">
+            <Text>{page.paragraph.text}</Text>
+          </Section>
 
-      {page.table && (
-        <Table.Root>
-          <Table.Header>
-            <Table.Row>
-              <Table.ColumnHeaderCell>{page.table.headers.questions.text}</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>{page.table.headers.correctAnswers.text}</Table.ColumnHeaderCell>
-              <Table.ColumnHeaderCell>{page.table.headers.givenAnswers.text}</Table.ColumnHeaderCell>
-            </Table.Row>
-          </Table.Header>
-          <Table.Body>
-            {page.table.rows.map((row, i) => (
-              <Table.Row key={i}>
-                <Table.Cell>{row.questions.text}</Table.Cell>
-                <Table.Cell>{row.correctAnswers.text}</Table.Cell>
-                <Table.Cell>{row.givenAnswers.text}</Table.Cell>
-              </Table.Row>
-            ))}
-          </Table.Body>
-        </Table.Root>
-      )}
+          {page.answers && (
+            <Section py="5">
+              <Grid columns="1" gap="2">
+                {page.answers.map((answer) => (
+                  <Button
+                    variant="solid"
+                    size="3"
+                    // @ts-expect-error "LSP does not recognize highContrast attribute"
+                    highContrast
+                    key={answer.id}
+                    onClick={() => handleUserAction({ answer: answer.id })}
+                  >
+                    {answer.text}
+                  </Button>
+                ))}
+              </Grid>
+            </Section>
+          )}
 
+          {page.table && (
+            <Table.Root>
+              <Table.Header>
+                <Table.Row>
+                  <Table.ColumnHeaderCell>{page.table.headers.questions.text}</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>{page.table.headers.correctAnswers.text}</Table.ColumnHeaderCell>
+                  <Table.ColumnHeaderCell>{page.table.headers.givenAnswers.text}</Table.ColumnHeaderCell>
+                </Table.Row>
+              </Table.Header>
+              <Table.Body>
+                {page.table.rows.map((row, i) => (
+                  <Table.Row key={i}>
+                    <Table.Cell>{row.questions.text}</Table.Cell>
+                    <Table.Cell>{row.correctAnswers.text}</Table.Cell>
+                    <Table.Cell>{row.givenAnswers.text}</Table.Cell>
+                  </Table.Row>
+                ))}
+              </Table.Body>
+            </Table.Root>
+          )}
 
-      {page.button && (
-        <Button onClick={() => handleUserAction({})}>
-          {page.button.text}
-        </Button>
-      )}
-    </Card>
+          {page.button && (
+            <Button
+              mt="auto"
+              mb="3"
+              variant="solid"
+              // @ts-expect-error "LSP does not recognize highContrast attribute"
+              highContrast
+              onClick={() => handleUserAction({})}>
+              {page.button.text}
+            </Button>
+          )}
+        </Flex>
+      </Card>
+    </Container>
   );
 };
--- services/react-renderer/root.tsx
+++ services/react-renderer/root.tsx
@@ -3,6 +3,12 @@
 // @ts-expect-error "CSS file import"
 import "@radix-ui/themes/styles.css";
 
+// @ts-expect-error "font import"
+import "@fontsource/roboto";
+
+// @ts-expect-error "CSS file import"
+import "@/services/react-renderer/styles.css";
+
 import { createRoot } from "react-dom/client";
 import { Theme } from "@radix-ui/themes";
 
@@ -13,7 +19,12 @@ const rootNode = document.getElementById("root");
 const root = createRoot(rootNode!);
 
 root.render(
-  <Theme>
+  <Theme
+    accentColor="gray"
+    grayColor="mauve"
+    panelBackground="solid"
+    radius="none"
+  >
     <Quiz data={QuizData}></Quiz>
   </Theme>
 );
